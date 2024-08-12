bbe3369dfb2194e28de9df5e0f008e45b568ccbc
zahiruddin <kepaknaga@gmail.com>
2024-08-12 20:42:53 +0800

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    
+    url = request.args.get("url")
+    target_url = url
+    response = requests.get(target_url, params=request.args)
+    return str(response.content)
+
+if __name__ == '__main__':
+	app.run(port=8000, debug=True, host='0.0.0.0')
