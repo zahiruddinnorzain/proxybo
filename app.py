f78c1784430243cb33143fa940e153c65efc352f
zahiruddin <kepaknaga@gmail.com>
2024-08-16 08:59:53 +0800

    fix byte

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def home():
     url = request.args.get("url")
     target_url = url
     response = requests.get(target_url, params=request.args)
-    return str(response.content)
+    output = response.content
+    output = output.decode('utf-8')
+    return output
 
 if __name__ == '__main__':
 	app.run(port=8000, debug=True, host='0.0.0.0')
